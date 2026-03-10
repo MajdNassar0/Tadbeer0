daa3ac59bfcf6a7b98769c7e72662f090b9ede61
MajdNassar0 <majdmnassar05@gmail.com>
2026-03-10 21:28:05 +0200

    feat ( Services Section ): Add Services Section to tadbeer .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import AboutSection from './components/AboutSection/AboutSection'
+import ServicesSection from './components/ServicesSection/ServicesSection'
 
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <ServicesSection/>
     <AboutSection />
     <Footer />
     
