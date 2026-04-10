0940a92eebea4a1882ee142acffe13f98d35e9e3
MajdNassar0 <majdmnassar05@gmail.com>
2026-04-10 18:10:41 +0300

    feat(ScrollToTop ): feat: implement ScrollToTop component and integrate with MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from "../components/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop"; 
 import { Outlet } from 'react-router-dom'
 
 function MainLayout() {
   return (
    <>
+   <ScrollToTop />
    <Navbar />
    <Outlet/>
    <Footer />
